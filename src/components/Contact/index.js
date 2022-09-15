684b0072b62841a60c682ed3ae87b06703c980d3
Tye Stanley <tyestanley090@gmail.com>
2022-09-15 11:45:49 -0700

    added contact page layout

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./contact.css";
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome } from '@fortawesome/free-solid-svg-icons';
+
+import Box from '@mui/material/Box';
+
+import FormControl from '@mui/material/FormControl';
+import FormHelperText from '@mui/material/FormHelperText';
+import InputLabel from '@mui/material/InputLabel';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import { TextField } from '@mui/material';
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
 
 function Contact() {
 
@@ -28,6 +38,16 @@ function Contact() {
       homeIcon.classList.remove('openIcon');
     }
   }
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleChange = (event) => {
+    if (event.target.value === name) {
+    setName(event.target.value);
+    }
+    console.log(event.target === name)
+  };
 
   return (
     <div id="contact" class="contact">
@@ -60,6 +80,93 @@ function Contact() {
 
         </div>
       </div>
+
+
+
+    <div className="content-details">
+      <div className="contact-details">
+        <h2>Send me a message!</h2>
+        <div className="contact-form">
+          <form>
+            <Box
+              component="form"
+              sx={{
+                '& > :not(style)': { m: 1 },
+                
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <FormControl fullWidth>
+                <InputLabel htmlFor="name-input">Name</InputLabel>
+                <OutlinedInput
+                  id="name-input"
+                  value={name}
+                  onChange={handleChange}
+                  label="Name"
+                />
+              </FormControl>
+            </Box>
+
+            <Box
+              component="form"
+              sx={{
+                '& > :not(style)': { m: 1 },
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <FormControl fullWidth>
+                <InputLabel htmlFor="email-input">Email</InputLabel>
+                <OutlinedInput
+                  id="email-input"
+                  value={email}
+                  onChange={handleChange}
+                  label="Name"
+                />
+              </FormControl>
+            </Box>
+          
+            <TextField 
+              sx={{
+                '& > :not(style)': { m: 1 },
+              }}
+              id="message-input" 
+              label="Message" 
+              variant="outlined" 
+              multiline
+              rows={7}
+              fullWidth
+            />
+          
+            <Button
+              sx={{
+                '& > :not(style)': { m: 1 },
+                marginTop: 2,
+                paddingRight: 1,
+                paddingTop: 0,
+                paddingBottom: 0,
+              }}
+              variant="contained"
+              color="primary"
+              size="meduim"
+              endIcon={<SendIcon />}
+            > 
+              send
+            </Button>
+
+          </form>
+        </div>
+        
+        <div className="contact-info">
+          <h3>Contact Info</h3>
+          <p>Email: tyestanley090@gmail.com</p>
+        </div>
+
+      </div>
+    </div>
+
+
 
       <div className="backdrop">
         <div className="backnav">
